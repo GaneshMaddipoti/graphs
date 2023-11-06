a9eb0f723cd9c530a05cba1f4cbcba83f97246f7
Atmakuri Madhavi <atmakurimadhavi1203@gmail.com>
2023-11-06 19:11:45 +0000

    Added to git

---
+++ src/main/resources/static/js/softSkills.js
@@ -0,0 +1,22 @@
+
+let softSkillsNodeDataArray = [
+
+    {key: "Soft Skills", color: "WhiteSmoke", isGroup: true, category: "tree", img: "assets/img/gen/user.svg"},
+    {key: "Start", group:"Soft Skills", category: "picTemplate",},
+
+    {key: "Listen", group:"Soft Skills", category: "picTemplate",},
+    {key: "Communicate", group:"Soft Skills", category: "picTemplate",},
+    {key: "Present", group:"Soft Skills", category: "picTemplate",},
+    {key: "Negotiate", group:"Soft Skills", category: "picTemplate",},
+
+
+
+];
+
+let softSkillsLinkDataArray = [
+    { name: "startToListen", from: "Start", to: "Listen", category: "simplelink"},
+    { name: "startToListen", from: "Start", to: "Communicate", category: "simplelink"},
+    { name: "startToListen", from: "Start", to: "Present", category: "simplelink"},
+    { name: "startToListen", from: "Start", to: "Negotiate", category: "simplelink"},
+
+];
