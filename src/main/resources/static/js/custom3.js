7c947bd30b7712913ef86cd9ba612b24b6622806
Atmakuri Madhavi <atmakurimadhavi1203@gmail.com>
2023-11-09 05:38:39 +0000

    Added to git

---
+++ src/main/resources/static/js/custom3.js
@@ -0,0 +1,347 @@
+let nodeDataArray = [];
+let linkDataArray = [];
+
+function showDetails(e, obj) {
+    const node = obj.part;
+    if(node) {
+        const diagram = node.diagram;
+        let details = node.data;
+        showToolTip(node, diagram, null);
+    } else {
+        hideToolTip();
+    }
+}
+
+function textStyle() {
+    return [
+        { margin: 0, width: 75, textAlign: "center", font: '500 14px Roboto, sans-serif', stroke: "WhiteSmoke"}
+    ];
+}
+
+function textStyleLong() {
+    return [
+        { margin: 0, textAlign: "center", font: '500 14px Roboto, sans-serif', stroke: "WhiteSmoke"}
+    ];
+}
+
+function itemStyle() {
+    return [
+        { textAlign: "center", font: '12px Roboto, sans-serif'}
+    ];
+}
+
+function subGraphExpanderButtonStyle() {
+    return [
+        {
+            width: 10, height: 10, margin: 5,
+            "_subGraphExpandedFigure": "MinusLine",
+            "_subGraphCollapsedFigure": "PlusLine",
+            // "_buttonFillNormal": "DarkSlateGray",
+            // "_buttonStrokeNormal": "DarkSlateGray",
+            // "_buttonFillOver": "DarkSlateGray",
+            "_buttonStrokeOver": "DarkSlateGray ",
+            // "ButtonBorder.fill": "DarkSlateGray"
+        }
+    ];
+}
+
+function doMouseOver(e) {
+    hideToolTip()
+}
+
+//Diagram
+var $ = go.GraphObject.make;
+let diagram = new go.Diagram("myDiagramDiv",{layout: $(go.TreeLayout,
+        { angle: 0, nodeSpacing: 50, layerSpacing: 50}), "undoManager.isEnabled": true, "linkReshapingTool": new OrthogonalLinkReshapingTool(),
+    mouseOver: doMouseOver,
+    click: hideToolTip ,
+});
+
+//Nodes
+const itemtemplates = new go.Map();
+itemtemplates.add("text",$(go.Panel, $(go.TextBlock, new go.Binding("text"))));
+
+function showToolTip(obj, diagram, tool) {
+    var pt = diagram.lastInput.viewPoint;
+    let toolTipDIV = document.getElementById("toolTipDIV");
+    if(toolTipDIV && obj.data.toolTipHTML) {
+        toolTipDIV.style.left = (pt.x + 250) + "px";
+        toolTipDIV.style.top = (pt.y + 100) + "px";
+        toolTipDIV.innerHTML = obj.data.toolTipHTML.replaceAll("\n", "<br/>");
+        toolTipDIV.style.display = "block";
+    } else {
+        hideToolTip();
+    }
+}
+
+function hideToolTip() {
+    var toolTipDIV = document.getElementById('toolTipDIV');
+    if(toolTipDIV) {
+        toolTipDIV.style.display = "none";
+    }
+}
+
+var myToolTip = $(go.HTMLInfo, {
+    show: showToolTip,
+    //hide: hideToolTip
+});
+
+const picTemplate =
+    $(go.Node, "Vertical", {padding: 0, margin: new go.Margin(0, 0, 0, 0),},
+        $(go.Picture,
+            { maxSize: new go.Size(30, 30), },
+            new go.Binding("source", "img")),
+        $(go.TextBlock, textStyle(),
+            { margin: new go.Margin(0, 0, 0, 0),
+                maxSize: new go.Size(50, 30),
+                isMultiline: false },
+            new go.Binding("text", "desc")),
+        { click: (e, obj) => showDetails(e, obj) }
+    );
+
+const simpletemplate =
+    $(go.Node, "Auto",{ fromSpot: go.Spot.AllSides,  toSpot: go.Spot.AllSides, isShadowed: false, shadowOffset: new go.Point(3, 3) },
+        $(go.Shape, new go.Binding("desiredSize", "size"),
+            new go.Binding("figure", "shape"), { strokeWidth: 1, stroke: "#555", fill:"Transparent" }, new go.Binding("stroke", "color")),
+        $(go.TextBlock, textStyle(), new go.Binding("text", "desc")),
+        { click: (e, obj) => showDetails(e, obj) }
+    );
+const simpleBorderLessTemplate =
+    $(go.Node, "Auto",{ fromSpot: go.Spot.AllSides,  toSpot: go.Spot.AllSides, isShadowed: false, shadowOffset: new go.Point(3, 3) },
+        $(go.Shape, new go.Binding("desiredSize", "size"),
+            new go.Binding("figure", "shape"), { strokeWidth: 0, fill:"Transparent" }, new go.Binding("stroke", "color")),
+        $(go.TextBlock, textStyleLong(), new go.Binding("text", "desc")),
+        { click: (e, obj) => showDetails(e, obj) }
+    );
+
+const simpleWithTooltiptemplate =
+    $(go.Node, "Auto",{ fromSpot: go.Spot.AllSides,  toSpot: go.Spot.AllSides, isShadowed: false, shadowOffset: new go.Point(3, 3) },
+        $(go.Shape, new go.Binding("desiredSize", "size"),
+            new go.Binding("figure", "shape"), { strokeWidth: 1, stroke: "#555", fill:"Transparent" }, new go.Binding("stroke", "color")),
+        $(go.TextBlock,textStyle(), new go.Binding("text", "desc")),
+        {toolTip: $("ToolTip", $(go.TextBlock, { margin: 4 }, new go.Binding("text", "desc")))},
+        { click: (e, obj) => showDetails(e, obj) }
+    );
+
+// the "detailed" template shows all of the information in a Table Panel
+const detailtemplate =
+    $(go.Node, "Auto", { fromSpot: go.Spot.AllSides,  toSpot: go.Spot.AllSides, isShadowed: false, shadowOffset: new go.Point(3, 3) },
+        $(go.Shape, new go.Binding("desiredSize", "size"),
+            new go.Binding("figure", "shape"), { strokeWidth: 1, stroke: "#555" }, new go.Binding("fill", "color")),
+        $(go.Panel, "Vertical",
+            $(go.TextBlock, textStyle(), new go.Binding("text", "desc")),
+            $(go.Panel, "Vertical", {defaultAlignment: go.Spot.Left, margin: 0,},
+                new go.Binding("itemArray", "items"),{
+                    itemTemplate:
+                        $(go.Panel, "Auto", {margin: 2 }, $(go.Shape, "RoundedRectangle", new go.Binding("fill", "color")),
+                            $(go.TextBlock, itemStyle(), new go.Binding("text", "text"))
+                        )
+                })),
+        { click: (e, obj) => showDetails(e, obj) }
+    );
+
+const templmap = new go.Map();
+templmap.add("simple", simpletemplate);
+templmap.add("simpleBL", simpleBorderLessTemplate);
+templmap.add("simpleTooltip", simpleWithTooltiptemplate)
+templmap.add("detailed", detailtemplate);
+templmap.add("picTemplate", picTemplate);
+templmap.add("", diagram.nodeTemplate);
+diagram.nodeTemplateMap = templmap;
+
+//Links
+const simplelinktemplate =
+    $(go.Link, {routing: go.Link.AvoidsNodes, reshapable: true, resegmentable: true, corner: 5},
+        $(go.Shape, { strokeWidth: 1, stroke: "WhiteSmoke" }),
+        $(go.Shape, { toArrow: "Standard" }, {stroke: "WhiteSmoke", fill: "WhiteSmoke"}),
+    );
+
+const byDirLinkTemplate =
+    $(go.Link, {routing: go.Link.AvoidsNodes, reshapable: true, resegmentable: true, corner: 5},
+        $(go.Shape, { strokeWidth: 1, stroke: "WhiteSmoke" }),
+        $(go.Shape, { fromArrow: "Backward" }, {stroke: "WhiteSmoke", fill: "WhiteSmoke"}),
+        $(go.Shape, { toArrow: "Standard" }, {stroke: "WhiteSmoke", fill: "WhiteSmoke"}),
+    );
+
+const thickLinkTemplate =
+    $(go.Link, {routing: go.Link.AvoidsNodes, reshapable: true, resegmentable: true, corner: 5},
+        $(go.Shape, { strokeWidth: 8, stroke: "WhiteSmoke" }),
+        $(go.Panel, "Auto",
+            $(go.Shape, "Rectangle", { fill: "lightgrey", stroke: "gray" }, new go.Binding("fill", "color")),
+            $(go.TextBlock, { margin: 3 }, new go.Binding("text", "channel"))),
+        {toolTip: $("ToolTip", $(go.TextBlock, { margin: 4 }, new go.Binding("text", "desc")))}
+    );
+
+const simplelinklabletemplate =
+    $(go.Link, {routing: go.Link.AvoidsNodes, reshapable: true, resegmentable: true, corner: 5},
+        $(go.Shape, { strokeWidth: 1, stroke: "WhiteSmoke" }),
+        $(go.Shape, { toArrow: "Standard" }, {fill: "WhiteSmoke"}),
+        $(go.Panel, "Auto",
+            $(go.Shape, "Rectangle", { fill: "lightgrey", stroke: "gray" }, new go.Binding("fill", "color")),
+            $(go.TextBlock, { margin: 3 }, new go.Binding("text", "channel"))),
+        {toolTip: $("ToolTip", $(go.TextBlock, { margin: 4 }, new go.Binding("text", "desc")))}
+    );
+const animatedLinkTemplate =
+    $(go.Link,
+        { routing: go.Link.AvoidsNodes, curve: go.Link.JumpGap, corner: 10, reshapable: true, toShortLength: 7 },
+        new go.Binding("points").makeTwoWay(),
+        // mark each Shape to get the link geometry with isPanelMain: true
+        $(go.Shape, { isPanelMain: true, stroke: "black", strokeWidth: 7 }),
+        $(go.Shape, { isPanelMain: true, stroke: "SteelBlue", strokeWidth: 5 }),
+        $(go.Shape, { isPanelMain: true, stroke: "white", strokeWidth: 3, name: "PIPE", strokeDashArray: [10, 10] }),
+        $(go.Shape, { toArrow: "Triangle", scale: 1.3, fill: "gray", stroke: null })
+    );
+
+const linktemplmap = new go.Map();
+linktemplmap.add("simplelink", simplelinktemplate);
+linktemplmap.add("byDirLink", byDirLinkTemplate);
+linktemplmap.add("thickLink", thickLinkTemplate);
+linktemplmap.add("simplelinklabel", simplelinklabletemplate);
+linktemplmap.add("animatedLink", animatedLinkTemplate);
+linktemplmap.add("", diagram.linkTemplate);
+diagram.linkTemplateMap = linktemplmap;
+
+
+diagram.groupTemplateMap.add("tree", $(go.Group, "Auto", {layout: $(go.TreeLayout,
+            { angle: 0, nodeSpacing: 30, layerSpacing: 50 }), isShadowed: false, shadowOffset: new go.Point(3, 3)},
+    $(go.Shape, "RoundedRectangle", // surrounds everything
+        { parameter1: 0, strokeWidth: 1, stroke: "#555", fill: "Transparent", strokeDashArray: [4, 2] }, new go.Binding("stroke", "color")),
+    $(go.Panel, "Vertical",  // position header above the subgraph
+        { defaultAlignment: go.Spot.Left },
+        $(go.Panel, "Horizontal",  // the header
+            { defaultAlignment: go.Spot.Right },
+            $(go.Picture,{ maxSize: new go.Size(30, 30) }, new go.Binding("source", "img")),
+            $(go.TextBlock, textStyle(), new go.Binding("text", "desc"),),
+            $("SubGraphExpanderButton", subGraphExpanderButtonStyle()),
+             { click: (e, obj) => showDetails(e, obj) }
+        ),
+        $(go.Placeholder,     // represents area for all member parts
+            { padding: new go.Margin(10, 10), background: "Transparent" }),
+    ), new go.Binding("isSubGraphExpanded", "expand"),
+));
+
+diagram.groupTemplateMap.add("treeBL", $(go.Group, "Auto", {layout: $(go.TreeLayout,
+            { angle: 0, nodeSpacing: 30, layerSpacing: 50 }), isShadowed: false, shadowOffset: new go.Point(3, 3)},
+    $(go.Shape, "RoundedRectangle", // surrounds everything
+        { parameter1: 0, strokeWidth: 0, stroke: "#555", fill: "Transparent" }, new go.Binding("stroke", "color")),
+    $(go.Panel, "Vertical",  // position header above the subgraph
+        { defaultAlignment: go.Spot.Left },
+        $(go.Panel, "Horizontal",  // the header
+            { defaultAlignment: go.Spot.Right },
+            $(go.Picture,{ maxSize: new go.Size(30, 30) }, new go.Binding("source", "img")),
+            $(go.TextBlock, textStyleLong(), new go.Binding("text", "desc"),),
+            $("SubGraphExpanderButton", subGraphExpanderButtonStyle()),
+             { click: (e, obj) => showDetails(e, obj) }
+        ),
+        $(go.Placeholder,     // represents area for all member parts
+            { padding: new go.Margin(10, 10), background: "Transparent" }),
+    ), new go.Binding("isSubGraphExpanded", "expand"),
+));
+
+diagram.groupTemplateMap.add("tree90", $(go.Group, "Auto", {layout: $(go.TreeLayout,
+            { angle: 90, nodeSpacing: 30, layerSpacing: 30 }), isShadowed: false, shadowOffset: new go.Point(3, 3)},
+    $(go.Shape, "RoundedRectangle", // surrounds everything
+        { parameter1: 0, strokeWidth: 1, stroke: "#555", fill: "Transparent", strokeDashArray: [4, 2] }, new go.Binding("stroke", "color")),
+    $(go.Panel, "Vertical",  // position header above the subgraph
+        { defaultAlignment: go.Spot.Left },
+        $(go.Panel, "Horizontal",  // the header
+            { defaultAlignment: go.Spot.Right },
+            $(go.Picture,{ maxSize: new go.Size(30, 30) }, new go.Binding("source", "img")),
+            $(go.TextBlock, textStyle(), new go.Binding("text", "desc"),),
+            $("SubGraphExpanderButton", subGraphExpanderButtonStyle()),
+        ),
+        $(go.Placeholder,     // represents area for all member parts
+            { padding: new go.Margin(10, 10), background: "Transparent" })
+    ), new go.Binding("isSubGraphExpanded", "expand"),
+));
+diagram.groupTemplateMap.add("grid", $(go.Group, "Auto", {layout: $(go.GridLayout, {
+            wrappingColumn: 3, alignment: go.GridLayout.Position,cellSize: new go.Size(1, 1), spacing: new go.Size(1,1)
+        }), isShadowed: false, shadowOffset: new go.Point(0, 0)},
+    $(go.Shape, "RoundedRectangle", // surrounds everything
+        { parameter1: 0, strokeWidth: 1, stroke: "#555", fill: "Transparent", strokeDashArray: [4, 2] }, new go.Binding("stroke", "color")),
+    $(go.Panel, "Vertical",  // position header above the subgraph
+        { defaultAlignment: go.Spot.Left },
+        $(go.Panel, "Horizontal",  // the header
+            { defaultAlignment: go.Spot.Left },
+            $(go.Picture,{ maxSize: new go.Size(30, 30) }, new go.Binding("source", "img")),
+            $(go.TextBlock, textStyle(), new go.Binding("text", "desc"),),
+            $("SubGraphExpanderButton", subGraphExpanderButtonStyle()),
+            { click: (e, obj) => showDetails(e, obj) }
+        ),
+        $(go.Placeholder,     // represents area for all member parts
+            { padding: new go.Margin(10, 10), background: "Transparent" })
+    ), new go.Binding("isSubGraphExpanded", "expand"),
+));
+diagram.groupTemplateMap.add("grid10", $(go.Group, "Auto", {layout: $(go.GridLayout, {
+            wrappingColumn: 10, alignment: go.GridLayout.Position,cellSize: new go.Size(1, 1), spacing: new go.Size(5,5)
+        }), isShadowed: false, shadowOffset: new go.Point(0, 0)},
+    $(go.Shape, "RoundedRectangle", // surrounds everything
+        { parameter1: 0, strokeWidth: 1, stroke: "#555", fill: "Transparent", strokeDashArray: [4, 2] }, new go.Binding("stroke", "color")),
+    $(go.Panel, "Vertical",  // position header above the subgraph
+        { defaultAlignment: go.Spot.Left },
+        $(go.Panel, "Horizontal",  // the header
+            { defaultAlignment: go.Spot.Left },
+            $(go.Picture,{ maxSize: new go.Size(30, 30) }, new go.Binding("source", "img")),
+            $(go.TextBlock, textStyle(), new go.Binding("text", "desc"),),
+            $("SubGraphExpanderButton", subGraphExpanderButtonStyle()),
+            { click: (e, obj) => showDetails(e, obj) }
+        ),
+        $(go.Placeholder,     // represents area for all member parts
+            { padding: new go.Margin(10, 10), background: "Transparent" })
+    ), new go.Binding("isSubGraphExpanded", "expand"),
+));
+diagram.groupTemplateMap.add("grid-congested", $(go.Group, "Auto", {layout: $(go.GridLayout, {
+            wrappingColumn: 3, alignment: go.GridLayout.Position, cellSize: new go.Size(0, 0), spacing: new go.Size(0,0)
+        }), isShadowed: false, shadowOffset: new go.Point(3, 3)},
+    $(go.Shape, "RoundedRectangle", // surrounds everything
+        { parameter1: 5, strokeWidth: 1, stroke: "#555", fill: "Transparent", strokeDashArray: [4, 2] }, new go.Binding("stroke", "color")),
+    $(go.Panel, "Vertical",  // position header above the subgraph
+        { defaultAlignment: go.Spot.Left },
+        $(go.Panel, "Horizontal",  // the header
+            { defaultAlignment: go.Spot.Left },
+            $(go.Picture,{ maxSize: new go.Size(30, 30) }, new go.Binding("source", "img")),
+            $(go.TextBlock, textStyle(), new go.Binding("text", "desc"),),
+            $("SubGraphExpanderButton", subGraphExpanderButtonStyle()),
+            { click: (e, obj) => showDetails(e, obj) }
+        ),
+        $(go.Placeholder,     // represents area for all member parts
+            { padding: new go.Margin(0, 0), background: "Transparent" })
+    ), new go.Binding("isSubGraphExpanded", "expand"),
+));
+diagram.scrollMode = go.Diagram.InfiniteScroll;
+var myAnimation = null;
+
+function updateAnimation(arg) {
+    var harray = [];
+    if(arg === "impala"){
+        harray = ["dstoi", "itoe", "itohs", "etohs", "etocr", "crtoc", "ctoch", "chtochi", "hstoh", "htoi", "chtoh", "citoi", "htoci", "citoic", "itot", "ttoo"];
+    } else if(arg === "dremio") {
+        harray = ["dstoi", "itoe", "itohs", "etohs", "etocr", "crtoc", "ctoch", "hstoh", "htod", "chtoh", "htocdb", "dtot", "ttoo", "htomt", "cdbtodc", "cdbtod"];
+    }
+    var arr = diagram.model.linkDataArray;
+    for (var i = 0; i < arr.length; i++) {
+        let pivot = arr[i];
+        if(harray.includes(pivot.name)) {
+            pivot.category = "animatedLink";
+        } else {
+            pivot.category = "simplelink";
+        }
+    };
+    diagram.model = new go.GraphLinksModel(nodeDataArray, arr);
+    if (myAnimation) myAnimation.stop();
+    // Animate the flow in the pipes
+    myAnimation = new go.Animation();
+    myAnimation.easing = go.Animation.EaseLinear;
+
+    diagram.links.each(link => {
+        if(link.category === "animatedLink")
+            myAnimation.add(link.findObject("PIPE"), "strokeDashOffset", 20, 0)
+    });
+    // Run indefinitely
+    myAnimation.runCount = Infinity;
+    myAnimation.start();
+    //diagram.commandHandler.zoomToFit();
+}
+function czoomTofFit() {
+    diagram.commandHandler.zoomToFit();
+}
---
+++ src/main/resources/static/js/java.js
@@ -0,0 +1,82 @@
+let languageEvolutionHTML = `Computer language innovation and development occurs for 2 fundamental reasons
+1). To adapt to changing environments.
+2). To implement refinements and improvements in the art of programming.
+`;
+let cBeforeJavaHTML = `The ‘c ’ language fundamentally changed the way programming was approached.
+The creation of ‘c’ was direct result of the need for structured, efficient, high level language that could replace assembly code.
+C was invented and implemented by Dennis Ritchie, on DEC PDP 11 running Unix-OS,in 1970’s.
+‘C’ formally standardized in 1989 by ANSI`;
+let cppBeforeJavaHTML = `With ‘c’ programming language, once a project reaches a certain size, its complexity increases and exceeds what a programmer can manage.
+ To solve this problem, a new way to program was invented, called object oriented programming(oop).
+ Oop is a methodology that helps organize complex programs through the use of inheritance, encapsulation and polymorphism.
+ ‘C++’ was invented by bjarne stroustop in 1979, at Bell Labs, newjersey,
+ stroustrup initially called the language ‘c-with classes’, in 1983 the name changed to ‘C++’.
+ C++ extends ‘C’ by adding object oriented features.
+`;
+let creationOfJavaHTML = `Prior to java, languages are platform dependent, that means
+Programs, that run on windows may not run on other platforms(linux,mac,solaris, etc..)
+In an attempt to find such a solution, Gosling and other colleagues began work on a portable,
+platform-independent language that could be used to produce code that would run on variety of cpu’s.
+At the same time, java was being implemented , www (or) Internet has taken a shape. The web or internet too demanded portable programs.’
+Java achieves platform independency through,  generating an intermediate code
+All the devices which are having interpreter(JVM) can run the same byte code.
+Java was conceived by james gosling and his team at sun micro systems in 1991.
+This language was initially called “OAK” , but was renamed to “JAVA” in 1995.
+Java derives much of the syntax from ‘C’ and ‘C++’.
+`;
+function javaContainer() {
+    nodeDataArray = [];
+    let javaContainer = {key: "Java", desc: "Java", color: "LightSteelBlue", isGroup: true, category: "tree", img: "assets/img/java/java.png"};
+
+    let javaIntroduction = {key: "Java Introduction", desc: "Introduction", isGroup: true, category: "treeBL", group: "Java", expand: false};
+
+    let languageEvolution = {key: "Language Evolution", desc: "Language Evolution", category: "simpleBL", group: "Java Introduction", toolTipHTML: languageEvolutionHTML};
+    let cBeforeJava = {key: "CBeforeJava", desc: "Birth of modern programming C", category: "simpleBL", group: "Java Introduction", toolTipHTML: cBeforeJavaHTML};
+    let cppBeforeJava = {key: "CPPBeforeJava", desc: "The need for c++", category: "simpleBL", group: "Java Introduction", toolTipHTML: cppBeforeJavaHTML};
+    let creationOfJava = {key: "Java Creation", desc: "The creation of java", category: "simpleBL", group: "Java Introduction", toolTipHTML: creationOfJavaHTML};
+
+    let javaBuzzwords = {key: "Java Buzzwords", desc: "Java Buzzwords", color: "WhiteSmoke", isGroup: true, category: "treeBL", group: "Java Introduction", expand: false};
+
+    let javaSimple = {key: "Java Simple", desc: "Simple", category: "simpleBL", group: "Java Buzzwords"};
+    let javaOO = {key: "Java Object Oriented", desc: "Object Oriented", category: "simpleBL", group: "Java Buzzwords"};
+    let javaRobust = {key: "Java Robust", desc: "Robust", category: "simpleBL", group: "Java Buzzwords"};
+    let javaMultiThreaded = {key: "Java Multi Threaded", desc: "Multi Threaded", category: "simpleBL", group: "Java Buzzwords"};
+    let javaPortable = {key: "Java Portable", desc: "Portable", category: "simpleBL", group: "Java Buzzwords"};
+    let javaArchNeutral = {key: "Java Architecture Neutral", desc: "Architecture Neutral", category: "simpleBL", group: "Java Buzzwords"};
+    let javaInterpreted = {key: "Java Interpreted", desc: "Interpreted", category: "simpleBL", group: "Java Buzzwords"};
+    let javaDistributed = {key: "Java Distributed", desc: "Distributed", category: "simpleBL", group: "Java Buzzwords"};
+    let javaSecured = {key: "Java Secured", desc: "Secured", category: "simpleBL", group: "Java Buzzwords"};
+    let javaDynamic = {key: "Java Dynamic", desc: "Dynamic", category: "simpleBL", group: "Java Buzzwords"};
+
+    nodeDataArray = nodeDataArray.concat(javaContainer);
+    nodeDataArray = nodeDataArray.concat(javaIntroduction);
+    nodeDataArray = nodeDataArray.concat(languageEvolution);
+    nodeDataArray = nodeDataArray.concat(cBeforeJava);
+    nodeDataArray = nodeDataArray.concat(cppBeforeJava);
+    nodeDataArray = nodeDataArray.concat(creationOfJava);
+    nodeDataArray = nodeDataArray.concat(javaBuzzwords);
+    nodeDataArray = nodeDataArray.concat(javaSimple);
+    nodeDataArray = nodeDataArray.concat(javaOO);
+    nodeDataArray = nodeDataArray.concat(javaRobust);
+    nodeDataArray = nodeDataArray.concat(javaMultiThreaded);
+    nodeDataArray = nodeDataArray.concat(javaPortable);
+    nodeDataArray = nodeDataArray.concat(javaArchNeutral);
+    nodeDataArray = nodeDataArray.concat(javaInterpreted);
+    nodeDataArray = nodeDataArray.concat(javaDistributed);
+    nodeDataArray = nodeDataArray.concat(javaSecured);
+    nodeDataArray = nodeDataArray.concat(javaDynamic);
+
+    let glModel = new go.GraphLinksModel(nodeDataArray,linkDataArray);
+    diagram.model = glModel;
+    return diagram;
+}
+
+function javaInto() {
+    //var diagram = javaContainer();
+    var data = diagram.model.findNodeDataForKey("Java Introduction");
+    if (data) {
+        diagram.model.startTransaction("modified property");
+        diagram.model.set(data, "expand", true);
+        diagram.model.commitTransaction("modified property");
+    }
+}
