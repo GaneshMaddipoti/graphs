15462896d423b2835b3ac0a7a7420ddf75acc16a
Atmakuri Madhavi <atmakurimadhavi1203@gmail.com>
2024-03-02 22:46:23 +0000

    updated

---
+++ src/main/resources/static/js/personalDevelopment.js
@@ -0,0 +1,24 @@
+let extremeOwnershipHTML = `
+Please take extreme ownership in <br/>
+1) Health           <br/>
+2) Wealth           <br/>
+3) Love             <br/>
+4) Happiness        <br/>
+`;
+
+let consciousVsSubconsciousHTML= `Conscious = Logical, Rational, Plan, Predict, Focus - Future based <br/>
+Subconscious = Automatic, Memory, Emotion, Paradigm, belief- - Presence based <br/>
+It is not programmed for change <br/>
+It is only look for food, water, shelter, clothes and remaining things it will not bother <br/>
+It fights with rapid change <br/>
+Make small change to start and make it happy <br/>`;
+
+let habitHTML = `Things we will repeatedly do become habits and will do subconsciously <br/>
+Character is build of habits from birth to end <br/>
+Habit consists - trigger, routine, reward <br/>
+If the habits are subconscious, its very difficult to change <br/>
+How to rewire subconscious mind <br/>
+1) Insert new routine and slowly expand <br/>`;
+
+let disciplineHTML = `Getting info from subconscious mind, and compare with long term goal <br/>
+and decide with conscious mind <br/>`;
